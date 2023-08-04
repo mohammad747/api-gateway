df56a020ebbc2d3edb55c73cb670421d4778956d
mohammad hashemi <mohammad.hashemi747@gmail.com>
2023-08-04 12:40:14 +0330

    Just lunch up the api-gateway and specifying the name, port and connecting to eureka naming server

---
+++ src/test/java/com/hashemi/microservice/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashemi.microservice.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
